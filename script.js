5f70a9de6cd387e8b322bdce569e3dac0aed0226
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-04-14 01:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", async function(){
+	const text = document.getElementById("text").value;
+	const delay = parseInt(document.getElementById("delay").value);
+	const outputDiv = document.getElementById("output");
+
+	outputDiv.textContent = '';
+
+	await showMessage(text, delay, outputDiv);
+});
+
+async function showMessage(message, delay, outputElement){
+	await new Promise(resolve => setTimeout(resolve, delay));
+	outputElement.textContent = message;
+}
